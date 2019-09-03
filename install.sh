7dc9809e378c1025db9f2a0e8188a311930fe41d
popunbom <fantom0779@gmail.com>
2019-09-04 00:01:38 +0900

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 git clone "https://github.com/popunbom/isucon-dotfiles" ~/.dotfiles
 
 for dotfile in $(find ~/.dotfiles -maxdepth 1 -type f -name ".*" | sed -E "s#$HOME/.dotfiles/(.*)#\1#g"); do
-  echo "ln -s ~/.dotfiles/${dotfile} ~/${dotfile}"
+  ln -sv ~/.dotfiles/${dotfile} ~/${dotfile}
 done
