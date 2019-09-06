af5a1d4a291b27184fd9807520c421753aa2c7b2
popunbom <fantom0779@gmail.com>
2019-09-06 13:22:26 +0900

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ GIT_DIR=".dotfiles"
 git clone "https://github.com/popunbom/isucon-dotfiles" ${GIT_DIR}
 
 for FILE_PATH in $(find ${GIT_DIR} -maxdepth 1 -type f -name ".*"); do
-  echo ln -sv "${FILE_PATH}" "${FILE_PATH#.dotfiles/}"
+  ln -sv "${FILE_PATH}" "${FILE_PATH#.dotfiles/}"
 done
